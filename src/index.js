e68b366db84087a40057caa27c9511f590d53d5c
Sergey <flargoin@gmail.com>
2025-05-19 19:01:36 +0300

    added layout for product page & install swiper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import './index.scss';
+import Swiper from 'swiper';
+import { EffectFade, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
+const thumbs = new Swiper('.slider-nav', {
+  direction: 'vertical',
+  slidesPerView: 3,
+  spaceBetween: 10,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+new Swiper('.slider-block', {
+  direction: 'vertical',
+  slidesPerView: 'auto',
+  modules: [EffectFade, Thumbs],
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  grabCursor: true,
+  loop: true,
+  thumbs: {
+    swiper: thumbs,
+  },
+});
 
 // import { checkOnlyNumbers } from './modules/helpers';
 // import product from './modules/product';
